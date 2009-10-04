c6f5c43db804ac537cfbfacb6f92e301b75a96d9
Matt Rude <github@mattrude.com>
2009-10-04 17:04:45 -0500

    made the copyright year auto update

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 		<a href="http://wordpress.org/"><img src="<?php bloginfo('template_url') ?>/images/ff_logo_only-1.png" height="100" width="100"></a>
 		<div id="footer_text" class="block">
 			<p>
-			Copyright © 2009, <a href="http://familyframeworks.com/">Family Frameworks, LLC</a><br />
+			Copyright &copy; <?php echo date("Y") ?>, <a href="http://familyframeworks.com/">Family Frameworks, LLC</a><br />
 			This page took <?php timer_stop(1); ?> seconds of computer labor to produce.<br />
 			Site Designed by <a href="http://mattrude.com/">Matt Rude</a>.<br />
 			<?php global $Panel; echo $Panel->Settings('FooterText'); ?><br />
