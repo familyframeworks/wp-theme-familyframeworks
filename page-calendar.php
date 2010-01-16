6d286ac6b5e3f2798c877dd8abc6f84379bd474d
Matt Rude <m@mattrude.com>
2010-01-16 15:24:57 -0600

    update to calendar

--- page-calendar.php
+++ page-calendar.php
@@ -1,17 +1,17 @@
 <?php /* Template Name: Calendar Page */ ?>
 <?php get_header(); ?>
-
-  <div class="hfeed content">
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-      <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-        <h1 class="attr"><?php the_title(); ?></h1>
-        <div class="entry">
-          <iframe width=100% height=600px src="http://www.google.com/calendar/embed?src=info%40familyframeworks.com"></iframe>
-        </div>
+<div class="hfeed content">
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+      <h1 class="attr"><?php the_title(); ?></h1>
+      <div class="entry">
+	<p>You must be logged into <a href="http://calendar.google.com" target="_blank">Google</a> in-order to view the content of this calender.</p>
+        <iframe width=100% height=600px frameborder=0 src="http://www.google.com/calendar/embed?src=info%40familyframeworks.com"></iframe>
+	<br />
+        <?php the_content(); ?>
       </div>
-    <?php endwhile; endif; ?>
-  </div>
-  <?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
-
-
+    </div>
+  <?php endwhile; endif; ?>
+</div>
+<?php edit_post_link('Edit this page.', '<p>', '</p>'); ?>
 <?php get_footer(); ?>
